99d08eb892ada712ff9434a635e402e8f6074bd0
Jeevanjot Singh <40867747+VitalJeevanjot@users.noreply.github.com>
2023-07-12 04:41:19 +0530

    Add project files.

---
+++ tutorial-maker-blazor-server/Data/Models/UserProjectModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace form_testor_blazor_server.Data.Models
+{
+    public class UserProjectModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set;}
+
+        [Required]
+        [MaxLength(250)]
+        public string CreatedByEmail { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string GUiD { get; set; }
+
+
+        [Required]
+        [MaxLength(250)]
+        public string URLiD { get; set; }
+
+
+    }
+}
---
+++ tutorial-maker-blazor-server/Data/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using form_testor_blazor_server.Data.Models;
+
+namespace form_testor_blazor_server.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<UserProjectModel> UserProjects { get; set; }
+
+
+    }
+}
